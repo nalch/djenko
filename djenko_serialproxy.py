31771c9b55f4a0df9f8002cc587689b6e21c1db0
nalch <Kristian.Scholze@web.de>
2014-09-21 23:07:24 +0200

    initial commit for serial communication

---
+++ djenko_serialproxy.py
@@ -0,0 +1,49 @@
+import json
+import sys
+import urllib2
+import time
+import serial
+
+# Configurations
+ping_server = 2
+jenkins_jobs=["MiXpress"]
+ser = serial.Serial('COM3', 9600)
+
+# Arduino Configuration
+SUCCESS = 'b'
+FAILURE = 'r'
+BUILDING = 'a'
+UNSTABLE = 'y'
+
+time.sleep(5)
+
+def get_status(jobName):
+    jenkinsUrl = "http://mwwebdev3:9400/job/"
+    try:
+        jenkinsStream   = urllib2.urlopen( jenkinsUrl + jobName + "/lastBuild/api/json" )
+    except urllib2.HTTPError, e:
+        print "URL Error: " + str(e.code)
+        print "      (job name [" + jobName + "] probably wrong)"
+        sys.exit(2)
+
+    try:
+        buildStatusJson = json.load( jenkinsStream )
+    except:
+        print "Failed to parse json"
+        sys.exit(3)
+
+    return jobName,buildStatusJson["timestamp"], buildStatusJson["result"],
+
+while(1):
+    for job in jenkins_jobs:
+        status = get_status(job)
+#        print status[0], status[2]
+        if status[2] == "UNSTABLE":
+            ser.write(UNSTABLE)
+        elif status[2] == "SUCCESS":
+            ser.write(SUCCESS)
+        elif status[2] == "FAILURE":
+            ser.write(FAILURE)
+        elif status[2] == None:
+            ser.write(BUILDING)
+        time.sleep(ping_server)

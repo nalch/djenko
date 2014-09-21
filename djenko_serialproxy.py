4e690bcb975691de4a50b864029269b8a76550a4
nalch <Kristian.Scholze@web.de>
2014-09-22 00:19:35 +0200

    ported serialproxy to linux settings

--- djenko_serialproxy.py
+++ djenko_serialproxy.py
@@ -6,8 +6,8 @@ import serial
 
 # Configurations
 ping_server = 2
-jenkins_jobs=["MiXpress"]
-ser = serial.Serial('COM3', 9600)
+jenkins_jobs=['testtask']
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 # Arduino Configuration
 SUCCESS = 'b'
@@ -18,32 +18,32 @@ UNSTABLE = 'y'
 time.sleep(5)
 
 def get_status(jobName):
-    jenkinsUrl = "http://mwwebdev3:9400/job/"
+    jenkinsUrl = "http://localhost:8080/job/"
     try:
-        jenkinsStream   = urllib2.urlopen( jenkinsUrl + jobName + "/lastBuild/api/json" )
+        jenkinsStream = urllib2.urlopen(jenkinsUrl + jobName + '/lastBuild/api/json')
     except urllib2.HTTPError, e:
         print "URL Error: " + str(e.code)
         print "      (job name [" + jobName + "] probably wrong)"
         sys.exit(2)
 
     try:
-        buildStatusJson = json.load( jenkinsStream )
+        buildStatusJson = json.load(jenkinsStream)
     except:
         print "Failed to parse json"
         sys.exit(3)
 
-    return jobName,buildStatusJson["timestamp"], buildStatusJson["result"],
+    return jobName, buildStatusJson["timestamp"], buildStatusJson["result"],
 
-while(1):
+while 1:
     for job in jenkins_jobs:
         status = get_status(job)
-#        print status[0], status[2]
+#        print job, status[0], status[2]
         if status[2] == "UNSTABLE":
             ser.write(UNSTABLE)
         elif status[2] == "SUCCESS":
             ser.write(SUCCESS)
         elif status[2] == "FAILURE":
             ser.write(FAILURE)
-        elif status[2] == None:
+        elif status[2] is None:
             ser.write(BUILDING)
         time.sleep(ping_server)
